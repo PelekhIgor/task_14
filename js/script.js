42980bbbc83ffd95c2894b7653c391685c820ec4
PelekhIgor <igorpelekh0112@gmail.com>
2022-05-31 01:01:52 +0300

    add class js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,66 @@
-const tabItem = document.querySelectorAll('.tab_item')
-const tabContent = document.querySelectorAll('.tab_content')
 
+class Tab{
+    tabs = []
+    texts = []
+    constructor(element) {
+        this.init(element)
+    }
 
-tabItem.forEach(function (e,i){
-    e.addEventListener('click', getClick)
+    init(element){
+        this.tabs = [...element.children[0].children]
+        this.texts = [...element.children[1].children]
+        element.children[0].addEventListener('click', this.onClick )
+    }
 
-function getClick(){
-    tabContent.forEach(function (content){
-        content.classList.remove('active')
-    })
-    tabItem.forEach(function(tab) {
-        tab.classList.remove('active')
-    })
-    tabContent[i].classList.add('active')
-    tabItem[i].classList.add('active')
+    onClick = (el) => {
+        this.el = this.tabs.indexOf(el.target)
+        this.activateEl(this.tabs, 'active')
+        this.activateEl(this.texts, 'active')
+    }
+
+    activateEl(elements,showClass) {
+        elements.forEach((e,i) =>{
+            if(i === this.el){
+                e.classList.add(showClass)
+            }else {
+                e.classList.remove(showClass)
+            }
+        })
+    }
+
 }
-})
+
+const tabsE = new Tab(document.querySelector('.tabs'))
+console.log(tabsE)
+
+
+
+
+
+
+
+
+
+
+
+
+// const tabItem = document.querySelectorAll('.tab_item')
+// const tabContent = document.querySelectorAll('.tab_content')
+//
+//
+// tabItem.forEach(function (e,i){
+//     e.addEventListener('click', getClick)
+//
+// function getClick(){
+//     tabContent.forEach(function (content){
+//         content.classList.remove('active')
+//     })
+//     tabItem.forEach(function(tab) {
+//         tab.classList.remove('active')
+//     })
+//     tabContent[i].classList.add('active')
+//     tabItem[i].classList.add('active')
+// }
+// })
 
 
